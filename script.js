8299683d33a8bc2f2340043043e08cce7ea18c84
habibadnaan <hello.habibadnaan@gmail.com>
2026-08-31 19:32:21 +0600

    Some functions in the all btn sections are added.

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let currenTabs = 'all'
+
+
+function switchTab(tab){
+console.log(tab);
+const tabs = ['all', 'interview', 'rejected']
+
+for( const t of tabs ){
+    const tabName= document.getElementById('tab-'+t);
+    console.log(tabName); 
+}
+
+
+}
